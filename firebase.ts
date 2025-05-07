cd6997832a4875c77f42090a1cfa437455e52b29
Batmagnai Munkhnasan <prime@Batmagnais-MacBook-Pro.local>
2025-05-07 12:32:10 +0800

    ENV UPDATE

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, FirebaseApp } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.YOUR_API_KEY,
-  authDomain: process.env.YOUR_AUTH_DOMAIN,
-  projectId: process.env.YOUR_PROJECT_ID,
-  storageBucket: process.env.YOUR_STORAGE_BUCKET,
-  messagingSenderId: process.env.YOUR_MESSAGING_SENDER_ID,
-  appId: process.env.YOUR_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 let firebaseApp: FirebaseApp;
